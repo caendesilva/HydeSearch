aecba06b41aa38e63e3b1b9b75f3c19d75c80d20
Caen De Silva <caen@desilva.se>
2022-05-26 22:26:30 +0200

    Create init and load methods

--- HydeSearch.ts
+++ HydeSearch.ts
@@ -1,7 +1,17 @@
 class HydeSearch {
     searchIndexLocation: string;
+    searchIndex;
 
     constructor(searchIndexLocation: string) {
         this.searchIndexLocation = searchIndexLocation;
+    }
+
+    public init(): void {
+        console.log("HS/Debug: Initializing...");
+        this.loadIndex();
+    }
+
+    public loadIndex(): void {
+        console.log("HS/Debug: Loading index...");
     }
 }
--- dist/HydeSearch.js
+++ dist/HydeSearch.js
@@ -2,4 +2,11 @@ class HydeSearch {
     constructor(searchIndexLocation) {
         this.searchIndexLocation = searchIndexLocation;
     }
+    init() {
+        console.log("HS/Debug: Initializing...");
+        this.loadIndex();
+    }
+    loadIndex() {
+        console.log("HS/Debug: Loading index...");
+    }
 }
