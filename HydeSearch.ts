3d539995456c69455c384d704713f1b1b7662417
Caen De Silva <caen@desilva.se>
2022-05-26 22:38:23 +0200

    Load the search index and assign it

--- HydeSearch.ts
+++ HydeSearch.ts
@@ -1,6 +1,6 @@
 class HydeSearch {
     searchIndexLocation: string;
-    searchIndex;
+    searchIndex: object;
 
     constructor(searchIndexLocation: string) {
         this.searchIndexLocation = searchIndexLocation;
@@ -8,10 +8,26 @@ class HydeSearch {
 
     public init(): void {
         console.log("HS/Debug: Initializing...");
-        this.loadIndex();
+        this.loadIndex().then(() => {
+            console.log(this.searchIndex);
+        });
     }
 
-    public loadIndex(): void {
+    public async loadIndex(): Promise<void> {
         console.log("HS/Debug: Loading index...");
+
+        const response = await fetch(this.searchIndexLocation, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`Could not load search index from ${this.searchIndexLocation}`);
+        }
+
+        this.searchIndex = await response.json();
+        console.log("HS/Debug: Index loaded.");
     }
 }
--- dist/HydeSearch.js
+++ dist/HydeSearch.js
@@ -1,12 +1,36 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 class HydeSearch {
     constructor(searchIndexLocation) {
         this.searchIndexLocation = searchIndexLocation;
     }
     init() {
         console.log("HS/Debug: Initializing...");
-        this.loadIndex();
+        this.loadIndex().then(() => {
+            console.log(this.searchIndex);
+        });
     }
     loadIndex() {
-        console.log("HS/Debug: Loading index...");
+        return __awaiter(this, void 0, void 0, function* () {
+            console.log("HS/Debug: Loading index...");
+            const response = yield fetch(this.searchIndexLocation, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            });
+            if (!response.ok) {
+                throw new Error(`Could not load search index from ${this.searchIndexLocation}`);
+            }
+            this.searchIndex = yield response.json();
+            console.log("HS/Debug: Index loaded.");
+        });
     }
 }
